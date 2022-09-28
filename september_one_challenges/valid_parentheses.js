d27840c6cd864f46fdc95838b9232e908178f918
Samuel Njenga <snjenga704@gmail.com>
2022-09-28 14:59:19 +0300

    feat: add valid_parentheses.js which returns whether an input string is valid or not

---
+++ september_one_challenges/valid_parentheses.js
@@ -0,0 +1,24 @@
+var isValid = function (s) {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i);
+    switch (c) {
+      case "(":
+        stack.push(")");
+        break;
+      case "[":
+        stack.push("]");
+        break;
+      case "{":
+        stack.push("}");
+        break;
+      default:
+        if (c !== stack.pop()) {
+          return false;
+        }
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid("()()"));
