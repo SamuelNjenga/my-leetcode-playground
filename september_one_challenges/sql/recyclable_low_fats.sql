ac3888efcce78023f1d7390743f1cbfc6773dad7
Samuel Njenga <snjenga704@gmail.com>
2022-09-29 14:38:55 +0300

    feat: add recyclable_low_fats.sql

---
+++ september_one_challenges/sql/recyclable_low_fats.sql
@@ -0,0 +1,5 @@
+/*Write an SQL query to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.*/
+
+select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
