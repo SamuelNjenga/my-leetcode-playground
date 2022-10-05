6cbdb5840432ab1f7af001defe1f227d7caa8c3a
Samuel Njenga <snjenga704@gmail.com>
2022-10-05 20:38:02 +0300

    feat: add ProductsDate.sql

---
+++ october_one_challenges/sql/ProductsDate.sql
@@ -0,0 +1,9 @@
+/*
+Write an SQL query to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+ */
+
+ select distinct sell_date, count(distinct product) as num_sold,group_concat(distinct product) as products from activities group by sell_date order by sell_date;
